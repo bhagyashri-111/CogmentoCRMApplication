a0ed26a330c440d23e4be73b291c37da093f8ae5
unknown <bhagyashri.jangale@gmail.com>
2024-07-19 23:36:05 +0530

    changes comitted by team lead

--- CogmentoCRM/src/test/java/RunnerTest/TestRunner.java
+++ CogmentoCRM/src/test/java/RunnerTest/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/Features",
-		glue="StepDefinition",
+		glue="Steps",
 		dryRun=false,
 		monochrome=true
 		
